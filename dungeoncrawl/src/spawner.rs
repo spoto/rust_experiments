1ffd5efb9b43dc403ce6324ca7c2a93d678f319f
Fausto Spoto <fausto.spoto@gmail.com>
2023-02-20 18:51:16 +0100

    Adding chasing monsters

--- dungeoncrawl/src/spawner.rs
+++ dungeoncrawl/src/spawner.rs
@@ -10,8 +10,8 @@ pub fn spawn_player(ecs: &mut World, pos: Point) {
                 glyph: to_cp437('@'),
             },
             Health {
-                current: 20,
-                max: 20,
+                current: 10,
+                max: 10,
             }
         )
     );
@@ -23,19 +23,34 @@ pub fn spawn_monster(ecs: &mut World, rng: &mut RandomNumberGenerator, pos: Poin
         _ => orc(),
     };
 
-    ecs.push(
-        (
-            Enemy,
-            pos,
-            Render {
-                color: ColorPair::new(WHITE, BLACK),
-                glyph,
-            },
-            MovingRandomly,
-            Health { current: hp, max: hp},
-            Name(name),
-        )
-    );
+    match rng.roll_dice(1, 10) {
+        1..=8 => ecs.push(
+            (
+                Enemy,
+                pos,
+                Render {
+                    color: ColorPair::new(WHITE, BLACK),
+                    glyph,
+                },
+                ChasingPlayer,
+                Health { current: hp, max: hp },
+                Name(name),
+            )
+        ),
+        _ => ecs.push(
+            (
+                Enemy,
+                pos,
+                Render {
+                    color: ColorPair::new(WHITE, BLACK),
+                    glyph,
+                },
+                MovingRandomly,
+                Health { current: hp, max: hp },
+                Name(name),
+            )
+        ),
+    };
 }
 
 fn goblin() -> (i32, String, FontCharType) {
