804606f4405b63b43e4659e7f204e36f4d0b81d1
Fausto Spoto <fausto.spoto@gmail.com>
2023-02-20 12:35:14 +0100

    Added combat

--- dungeoncrawl/src/systems/player_input.rs
+++ dungeoncrawl/src/systems/player_input.rs
@@ -1,8 +1,10 @@
 use crate::prelude::*;
 
 #[system]
-#[write_component(Point)]
+#[read_component(Point)]
 #[read_component(Player)]
+#[read_component(Enemy)]
+#[write_component(Health)]
 pub fn player_input(
     ecs: &mut SubWorld,
     commands: &mut CommandBuffer,
@@ -19,14 +21,43 @@ pub fn player_input(
             _ => Point::new(0, 0),
         };
 
+        let mut did_something = false;
+        let mut players = <(Entity, &Point)>::query()
+            .filter(component::<Player>());
+        let mut enemies = <(Entity, &Point)>::query()
+            .filter(component::<Enemy>());
+        let (player, destination) = players.iter(ecs)
+            .find_map(|(entity, pos)| Some((*entity, *pos + delta)))
+            .unwrap();
+
         if delta.x != 0 || delta.y != 0 {
-            let mut players = <(Entity, &Point)>::query()
-                .filter(component::<Player>());
-            players.iter(ecs).for_each(|(entity, pos)| {
-                let destination = *pos + delta;
-                commands.push(((), WantsToMove{ entity: *entity, destination }));
-                *turn_state = TurnState::PlayerTurn;
-            });
+            let mut hit_something = false;
+
+            enemies.iter(ecs)
+                .filter(|(_, pos)| {
+                    **pos == destination
+                })
+                .for_each(|(enemy, _)| {
+                    hit_something = true;
+                    did_something = true;
+                    commands.push(((), WantsToAttack { attacker: player, victim: *enemy }));
+                });
+
+            if !hit_something {
+                did_something = true;
+                commands.push(((), WantsToMove { entity: player, destination }));
+            }
         }
+
+        if !did_something {
+            if let Ok(mut health) = ecs
+                .entry_mut(player)
+                .unwrap()
+                .get_component_mut::<Health>()
+            {
+                health.current = i32::min(health.max, health.current+1);
+            }
+        }
+        *turn_state = TurnState::PlayerTurn;
     }
 }
