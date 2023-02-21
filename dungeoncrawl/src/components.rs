00d5fa76867144f3da771685054c260a21e41acc
Fausto Spoto <fausto.spoto@gmail.com>
2023-02-21 15:53:57 +0100

    Added amulet of Yala

--- dungeoncrawl/src/components.rs
+++ dungeoncrawl/src/components.rs
@@ -19,6 +19,12 @@ pub struct MovingRandomly;
 pub struct ChasingPlayer;
 
 #[derive(Clone, Copy, Debug, PartialEq)]
+pub struct Item;
+
+#[derive(Clone, Copy, Debug, PartialEq)]
+pub struct AmuletOfYala;
+
+#[derive(Clone, Copy, Debug, PartialEq)]
 pub struct WantsToMove {
     pub entity: Entity,
     pub destination: Point,
