62c300ee335b616b9ded63633ba1f0c281288241
Fausto Spoto <fausto.spoto@gmail.com>
2023-03-04 15:49:02 +0400

    Added drunkard and prefabricated map builders

--- dungeoncrawl/src/map_builder/mod.rs
+++ dungeoncrawl/src/map_builder/mod.rs
@@ -2,10 +2,14 @@ use crate::prelude::*;
 use empty::EmptyArchitect;
 use rooms::RoomsArchitect;
 use automata::CellularAutomataArchitect;
+use drunkard::DrunkardsWalkArchitect;
+use prefab::apply_prefab;
 
 mod empty;
 mod rooms;
 mod automata;
+mod drunkard;
+mod prefab;
 
 trait MapArchitect {
     fn new(&mut self, rng: &mut RandomNumberGenerator) -> MapBuilder;
@@ -23,8 +27,14 @@ pub struct MapBuilder {
 
 impl MapBuilder {
     pub fn new(rng: &mut RandomNumberGenerator) -> Self {
-        let mut architect = CellularAutomataArchitect{};
-        architect.new(rng)
+        let mut architect: Box<dyn MapArchitect> = match rng.range(0, 3) {
+            0 => Box::new(DrunkardsWalkArchitect{}),
+            1 => Box::new(RoomsArchitect{}),
+            _ => Box::new(CellularAutomataArchitect{}),
+        };
+        let mut mb = architect.new(rng);
+        apply_prefab(&mut mb, rng);
+        mb
     }
 
     fn fill(&mut self, tile: TileType) {
